19304d4f67bd7e22af4a6de224db294fda78da2b
Samuel Robison <4samuelr05@gmail.com>
2026-01-07 10:32:50 -0500

    feat: add feedback loop so agent can iterate on tool results

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 import argparse
+import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
+from config import MAX_ITERS
 from call_function import available_functions, call_function
 
 def main():
@@ -32,7 +34,18 @@ def main():
 
     #store the prompt and generate the response
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    generate_response(client, messages, args.verbose)
+    for _ in range(MAX_ITERS):
+        try:
+            response = generate_response(client, messages, args.verbose)
+            if response:
+                print()
+                print(response)
+                return
+        except Exception as e:
+            print(f"Error when generating response: {e}")
+
+    print(f"Maximum interations {MAX_ITERS} for calling agent reached")
+    sys.exit(1)
 
 def generate_response(client, messages, verbose):
     response = client.models.generate_content(model="gemini-2.5-flash", contents=messages, config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt,))
@@ -44,10 +57,16 @@ def generate_response(client, messages, verbose):
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    if not response.function_calls:
-        print(f"Response: {response.text}")
-        return
+    #track conversation history
+    if response.candidates:
+        for candidate in response.candidates:
+            if candidate.content:
+                messages.append(candidate.content)
 
+    if not response.function_calls:
+        return f"Response: {response.text}"
+
+    #handle and track function calling
     function_results = []
     for function_call in response.function_calls:
         print(f"Calling function: {function_call.name}({function_call.args})")
@@ -61,6 +80,8 @@ def generate_response(client, messages, verbose):
         if verbose:
             print(f"-> {result.parts[0].function_response.response}")
         function_results.append(result.parts[0])
+
+    messages.append(types.Content(role="user", parts=function_results))
 
 
 if __name__ == "__main__":
